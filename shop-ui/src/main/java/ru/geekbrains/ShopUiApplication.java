fd3341fc6d1c2571e37d86ee15d2b68439565b1e
DiNikOs <demon22odn08@rambler.ru>
2020-02-11 13:23:09 +0300

    + shop-ui MVC module

---
+++ shop-ui/src/main/java/ru/geekbrains/ShopUiApplication.java
@@ -0,0 +1,17 @@
+package ru.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//
+//@EnableDiscoveryClient
+@SpringBootApplication(exclude = SessionAutoConfiguration.class)
+public class ShopUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopUiApplication.class, args);
+    }
+
+}
+
